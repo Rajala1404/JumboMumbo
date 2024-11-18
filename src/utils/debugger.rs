75b90f676391ba21b24d66afe0cbd186311b74c5
Rajala1404 <115874847+Rajala1404@users.noreply.github.com>
2024-11-18 20:44:40 +0100

    Improved Camera collider debugger

--- src/utils/debugger.rs
+++ src/utils/debugger.rs
@@ -1,14 +1,17 @@
 use macroquad::color::{DARKPURPLE, WHITE};
-use macroquad::prelude::{draw_rectangle, screen_height, screen_width};
+use macroquad::prelude::{screen_height, screen_width};
+use macroquad::shapes::draw_rectangle_lines;
 use macroquad::text::draw_text;
 use macroquad_platformer::World;
 use crate::logic::player::Player;
 
 pub async fn draw_camera_collider(world: &World, player: &Player) {
+    let x_offset =  screen_width() / 60.0;
+    let y_offset = screen_height() - screen_height() / 15.0;
     let pos = world.actor_pos(player.camera_collider[0]);
-    draw_rectangle(pos.x, pos.y, screen_width() / 4.0, screen_height(), DARKPURPLE);
-    draw_text("Camera collider 0", pos.x, pos.y, screen_height() / 20.0, WHITE);
+    draw_rectangle_lines(pos.x, pos.y , screen_width() / 4.0, screen_height(), screen_height() / 20.0, DARKPURPLE);
+    draw_text("Camera collider 0", pos.x + x_offset, pos.y + y_offset, screen_height() / 20.0, WHITE);
     let pos = world.actor_pos(player.camera_collider[1]);
-    draw_rectangle(pos.x, pos.y, screen_width() / 4.0, screen_height(), DARKPURPLE);
-    draw_text("Camera collider 1", pos.x, pos.y, screen_height() / 20.0, WHITE);
+    draw_rectangle_lines(pos.x, pos.y, screen_width() / 4.0, screen_height(), screen_height() / 20.0, DARKPURPLE);
+    draw_text("Camera collider 1", pos.x + x_offset, pos.y + y_offset, screen_height() / 20.0, WHITE);
 }
